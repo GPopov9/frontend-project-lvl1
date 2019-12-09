672e575a9bc32f84337f297052b05a302b95a7f0
George <g.popov44@icloud.com>
2019-12-09 21:07:28 +0300

    removed Message fuction. Task#11.

--- src/engine.js
+++ src/engine.js
@@ -1,24 +1,24 @@
 import { car, cdr } from '@hexlet/pairs';
 
 import readlineSync from 'readline-sync';
-import { welcomeMessage, anyMessage } from './index';
 
 export default (message, func) => {
-  welcomeMessage();
-  anyMessage(message);
+  console.log('Welcome to the Brain Games!');
+  console.log(message);
   const name = readlineSync.question('May I have your name? ');
-  anyMessage(`Hello, ${name}!\n`);
+  console.log(`Hello, ${name}!`);
+  console.log('');
   for (let i = 0; i < 3; i += 1) {
     const pair = func();
-    anyMessage(`Question: ${car(pair)}`);
+    console.log(`Question: ${car(pair)}`);
     const answer = readlineSync.question('Your answer: ');
 
     if (answer === cdr(pair)) {
-      anyMessage('Correct!');
+      console.log('Correct!');
     } else {
-      anyMessage(`'${answer}' is wrong answer ;(. Correct answer was '${cdr(pair)}'.\nLet's try again, ${name}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${cdr(pair)}'.\nLet's try again, ${name}!`);
       return;
     }
   }
-  anyMessage(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!`);
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-// 1. Welcome message
-export const welcomeMessage = () => console.log('Welcome to the Brain Games!');
 
-// 2. Message With game description
-export const anyMessage = (message) => console.log(message);
