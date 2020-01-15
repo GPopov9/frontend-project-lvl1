5005fa8da7f044f5553bce3a46819534f19c4062
George <g.popov44@icloud.com>
2020-01-15 23:59:25 +0300

    refactor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,7 +7,7 @@ const progressionLength = 10;
 
 const findProgressionItem = (a, b, i) => a + b * i;
 
-const getProgressionQuestion = (startItem, progressionStep, secretPosition) => {
+const getQuestion = (startItem, progressionStep, secretPosition) => {
   let question = '';
   for (let i = 0; i < progressionLength; i += 1) {
     const progressionItem = startItem + progressionStep * i;
@@ -24,7 +24,7 @@ const makeDataForProgressionGame = () => {
   const randomStart = randomNumber(startNumber, endNumber);
   const randomStep = randomNumber(startNumber, endNumber);
   const randomPosition = randomNumber(startNumber, progressionLength - 1);
-  const gameQuestion = getProgressionQuestion(randomStart, randomStep, randomPosition);
+  const gameQuestion = getQuestion(randomStart, randomStep, randomPosition);
   const rightAnswer = String(findProgressionItem(randomStart, randomStep, randomPosition));
   return cons(gameQuestion, rightAnswer);
 };
